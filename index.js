43e938a43201c0afcade16cc24b64ebeb9809350
KalidindiAmitRaja <mahendraamit3@gmail.com>
2019-02-21 20:57:18 +0530

    update

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ app.use(bodyparser.urlencoded({ extended : true }));
 var path = require('path');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-app.listen((process.env.PORT || 8000),function(){
-	console.log("server is running");
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
 })
 app.post('/webhook',function(req,res) {
 	if(!req.body) return res.sendStatus(400);
 	res.setHeader('Content-Type','application/json');
-    var city = req.body.queryResult.parameters['geo-city'];
-    var w = getWeather(city);
-    let response = "";
-    let responseObj = {
-    						"fulfillmentText" :response,
-    						"fulfillmentMessages" : [{"text" : {"text" :[w]}}],
-    						"source":""
-    				  }
+    	var city = req.body.queryResult.parameters['geo-city'];
+    	var w = getWeather(city);
+    	let response = "";
+    	let responseObj = {
+    				"fulfillmentText" :response,
+    				"fulfillmentMessages" : [{"text" : {"text" :[w]}}],
+    				"source":""
+    			  }
     	return res.json(responseObj);
     }
 )
@@ -29,14 +29,14 @@ function cb (err,response,body){
 	if(err){
 	}
 	var weather = JSON.parse(body)
-    if(weather.message === "city not found")
-    {
-       	result = "Unable to get weather" + weather.message;
-    }
-    else{
-        result="Right now it is " + weather.main.temp + " degrees with " +weather.weather[0].description;
-    }
-    return result;
+    	if(weather.message === "city not found")
+    	{
+       		result = "Unable to get weather" + weather.message;
+    	}
+    	else{
+        	result="Right now it is " + weather.main.temp + " degrees with " +weather.weather[0].description;
+    	}
+    	return result;
 }
 function getWeather(city){
 	result = undefined;
