3393eca4025bfb25cc38734cc22c1bcfb6169a10
KalidindiAmitRaja <mahendraamit3@gmail.com>
2019-02-28 10:35:36 +0530

    update

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var http = require('http');
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var express = require('express');
 var request = require('request');
 var app = express();
 var w;
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
 app.post('/webhook',function(req,res) {
-	res.write("helloworld");
 	if(!req.body) return res.sendStatus(400);
 	res.setHeader('Content-Type','application/json');
     	var city = req.body.queryResult.parameters['geo-city'];
