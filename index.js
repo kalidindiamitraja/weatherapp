e9d9f863aa199c3fc4315c2ba5d5b5148945f358
KalidindiAmitRaja <mahendraamit3@gmail.com>
2019-03-10 10:13:37 +0530

    update

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var app = express();
 var w,city;
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.post('/webhook',function(err,req,res){
-	if(err) throw err;
 	console.log("into the post");
 	res.setHeader('Content-Type','application/json');
 	city = req.body.queryResult.parameters['geo-city'];
