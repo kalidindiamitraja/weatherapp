27e44931e2063bf37c94c2cab5f03e704aa66625
KalidindiAmitRaja <mahendraamit3@gmail.com>
2019-03-10 11:30:56 +0530

    update

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/', urlencodedParser, function (req, res) {
  var w = myfunc();
  res.end(w);
 })
-app.listen(process.env.PORT || 3000 , function(){
+server.listen(process.env.PORT || 3000 , function(){
 	console.log("port 3000 running");
 });
 
